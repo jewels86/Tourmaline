2c91b2a20f12eecc594e6b8297edd91b07385745
Jewels <jewelsytoffcial@gmail.com>
2024-04-09 20:29:24 -0400

    0.1
    - New versioning (minor++ for updates, patch++ for patches)
    - Completed Brute command and agent
    - Full application

--- Tourmaline/Scripts/BruteAgent.cs
+++ Tourmaline/Scripts/BruteAgent.cs
@@ -8,7 +8,60 @@ namespace Tourmaline.Scripts
 {
     public class BruteAgent
     {
-        public string Path { get; set; }
+        public string URL { get; set; }
+        public string WordlistPath { get; set; }
+        public string? OutfilePath { get; set; }
+        public bool DevMode { get; set; } = false;
 
+        internal BruteAgent(string wordlistPath, string url)
+        {
+            WordlistPath = wordlistPath;
+            URL = url ;
+        }
+
+        internal async Task<List<Path>> Start(Action<Path>? next = null)
+        {
+            List<Path> output = new();
+            HttpClient client = new();
+            List<string> paths;
+
+            string _tmp = URL;
+            ProcessURL(ref _tmp, true);
+            URL = _tmp;
+
+            var file = await File.ReadAllLinesAsync(WordlistPath);
+            paths = new(file);
+
+            foreach (var path in paths)
+            {
+                HttpResponseMessage res = await client.GetAsync($"{URL}/{path}");
+                if (!((int)res.StatusCode < 400)) continue;
+
+                Path pathOutput = new();
+                pathOutput.URL = $"{URL}/{path}";
+                pathOutput.Type = res.Content.Headers.ContentType?.MediaType ?? "unknown";
+                pathOutput.Status = (int)res.StatusCode;
+
+                output.Add(pathOutput);
+
+                next?.Invoke(pathOutput);
+            }
+
+            return output;
+        }
+
+        internal void ProcessURL(ref string url, bool isBaseURL = false)
+        {
+            if (url.StartsWith("/") && !isBaseURL)
+            {
+                url = URL + url;
+            }
+
+            url = url.StartsWith("http://") || url.StartsWith("https://") ? url : "http://" + url;
+
+            string[] parts = url.Split('#', '?');
+            url = parts[0];
+
+        }
     }
 }
--- Tourmaline/Scripts/BruteCommand.cs
+++ Tourmaline/Scripts/BruteCommand.cs
@@ -11,11 +11,28 @@ namespace Tourmaline.Scripts
     {
         public sealed class Settings : CommandSettings
         {
+            [CommandArgument(0, "<url>")]
+            public string URL { get; set; }
+            [CommandArgument(1, "<wordlistPath>")]
+            public string WordlistPath { get; set; }
 
+            [CommandOption("-d")]
+            public bool? DevMode { get; set; }
         }
 
         public async override Task<int> ExecuteAsync(CommandContext context, Settings settings)
         {
+            BruteAgent agent = new(settings.WordlistPath, settings.URL);
+            //if (settings.MaxPaths != null) agent.MaxPaths = (int)settings.MaxPaths;
+            if (settings.DevMode != null && settings.DevMode == true) agent.DevMode = (bool)settings.DevMode;
+
+            GUI gui = new();
+
+            gui.Start();
+            await agent.Start((path) => gui.AddRow(path.URL, path.Type, path.Status.ToString()));
+
+            gui.TaskCompletionSource.TrySetResult();
+
             return 0;
         }
     }
